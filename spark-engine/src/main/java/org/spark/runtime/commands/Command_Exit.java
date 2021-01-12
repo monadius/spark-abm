4c195a36e7b93efe6e7fb61a01afd9314e53c1da
Alexey Solovyev <solovyev.alexey@gmail.com>
2021-01-11 23:15:04 -0800

    SPARK Internal -> spark-engine

---
+++ spark-engine/src/main/java/org/spark/runtime/commands/Command_Exit.java
@@ -0,0 +1,24 @@
+package org.spark.runtime.commands;
+
+import org.sparkabm.core.SparkModel;
+import org.spark.runtime.internal.engine.AbstractSimulationEngine;
+
+/**
+ * Tells the server to stop simulations and exit
+ * @author Monad
+ *
+ */
+@SuppressWarnings("serial")
+public class Command_Exit extends ModelManagerCommand {
+	public Command_Exit() {
+	}
+
+	@Override
+	public void execute(SparkModel model, AbstractSimulationEngine engine)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
---
+++ spark-engine/src/main/java/org/spark/runtime/commands/Command_Stop.java
@@ -0,0 +1,27 @@
+package org.spark.runtime.commands;
+
+import org.sparkabm.core.SparkModel;
+import org.spark.runtime.internal.engine.AbstractSimulationEngine;
+
+/**
+ * Tells the server to stop the current simulation (if any)
+ * @author Monad
+ *
+ */
+@SuppressWarnings("serial")
+public class Command_Stop extends ModelManagerCommand {
+	public Command_Stop() {
+	}
+	
+	@Override
+	public void execute(SparkModel model, AbstractSimulationEngine engine)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "Stop";
+	}
+}
