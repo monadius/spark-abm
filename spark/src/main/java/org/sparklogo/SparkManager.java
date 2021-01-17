07141549f3fc346dd1fd41f6db5a13d9de7b1fb2
Alexey Solovyev <solovyev.alexey@gmail.com>
2021-01-16 20:36:29 -0800

    spark: main class

---
+++ spark/src/main/java/org/sparklogo/SparkManager.java
@@ -0,0 +1,9 @@
+package org.sparklogo;
+
+import org.sparklogo.gui.MainFrame;
+
+public class SparkManager {
+    public static void main(String[] args) throws Exception {
+        new MainFrame().setVisible(true);
+    }
+}
